3be6641245b840b2be868c877564112c21d70222
AirKuma <401violet@gmail.com>
2018-05-27 11:44:51 +0800

    dev-test

--- Hostel/src/DBHostel/DBConstants.java
+++ Hostel/src/DBHostel/DBConstants.java
@@ -4,5 +4,5 @@ public interface DBConstants {
 	String DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";
 	String URI = "jdbc:odbc:hostel";
 	String USERNAME = "fourinone";
-	String PASSWORD = "hostel";   //dev_test
+	String PASSWORD = "hostel";   //dev_test 01
 }
